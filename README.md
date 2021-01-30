1f35c1ba1827e247cca79ff58e2e8f60d90495ab
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-30 23:14:37 +0700

    BroadcastReceiver trong Android Part 2 (Dynamic Receivers) - [Android Tutorial - #19]

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TinCoder_BroadcastReceiverPart1
 https://www.youtube.com/watch?v=Yv5YPO_I4Ts&amp;list=PL3Ob3F0T-08be16h_I98K7lB7ba70q6bi
+https://www.youtube.com/watch?v=AmEhEzceuDU
---
+++ app/src/main/java/com/example/broadcastreceiverpart1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.broadcastreceiverpart1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ExampleBroadcastReceiver exampleBroadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        exampleBroadcastReceiver = new ExampleBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(exampleBroadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+//        unregisterReceiver(exampleBroadcastReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(exampleBroadcastReceiver);
+    }
+}
